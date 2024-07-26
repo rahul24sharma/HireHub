6feb2b0fcb88752e82af8ac1a25c940bcd3ff7e8
rahul sharma <109339946+rahul24sharma@users.noreply.github.com>
2024-07-26 12:51:02 +0530

    Update6

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 // vite.config.js
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
